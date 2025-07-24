b413131b7b3d7f0c2976a0e780dca1f9f85586fe
Tushar Sain <skytusharkum786@gmail.com>
2025-07-24 20:50:51 +0530

    Update paypal-return.jsx

--- client/src/pages/shopping-view/paypal-return.jsx
+++ client/src/pages/shopping-view/paypal-return.jsx
@@ -2,39 +2,45 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useLocation } from "react-router-dom";
 import { capturePayment } from "../../Store/shop/product-slice/order-slice/order-slice";
-import { CardTitle,CardHeader,Card } from "../../Component/ui/all";
+import { CardTitle, CardHeader, Card } from "../../Component/ui/all";
 
+function PaypalReturnPage() {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const paymentId = params.get("paymentId");
+  const payerId = params.get("PayerID");
 
-function  PaypalReturnPage(){
-const dispatch=useDispatch();
-const location=useLocation();
-const params=new URLSearchParams(location.search);
-const paymentId=params.get('paymentId')
-const payerId=params.get('PayerID')
+  useEffect(() => {
+    const orderId = sessionStorage.getItem("currentOrderId");
 
-useEffect(() => {
-  if (paymentId && payerId) {
-    const orderId = JSON.parse(sessionStorage.getItem('currentOrderId'));
+    if (paymentId && payerId && orderId) {
+      dispatch(
+        capturePayment({
+          paymentId,
+          payerId,
+          orderId: JSON.parse(orderId),
+        })
+      ).then((data) => {
+        if (data?.payload?.success) {
+          sessionStorage.removeItem("currentOrderId");
+          window.location.href = "/shop/payment-success";
+        } else {
+          window.location.href = "/shop/payment-failed";
+        }
+      });
+    } else {
+      window.location.href = "/shop/payment-failed";
+    }
+  }, [paymentId, payerId, dispatch]);
 
-    dispatch(capturePayment({ paymentId, payerId, orderId })).then(data => {
-      if (data?.payload?.success) {
-        sessionStorage.removeItem('currentOrderId');
-        window.location.href = '/shop/payment-success';
-      }
-    });
-  }
-}, [paymentId, payerId, dispatch]);
-
-    return(
-        <Card>
-            <CardHeader>
-                <CardTitle>
-                    Processing payment Please wait
-                </CardTitle>
-            </CardHeader>
-        </Card>
-      
-    )
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Processing payment. Please wait...</CardTitle>
+      </CardHeader>
+    </Card>
+  );
 }
 
 export default PaypalReturnPage;
